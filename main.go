92a33e3dab5555dee6215099326095409259c26e
Daiva Nabil Praseco <daiva.np@it.sigma-tech.co.id>
2023-12-20 15:16:36 +0700

    remove comment

--- main.go
+++ main.go
@@ -9,21 +9,10 @@ import (
 
 func main() {
 	db := config.ConnectDatabase()
-	// bookRepository := domain.NewRepository(db)
-	// bookService := service.NewService(bookRepository)
-	// bookHandler := handler.NewBookHandler(bookService)
 
 	router := gin.Default()
 
 	route.ApiRoute(router, db)
-
-	// books := router.Group("/books")
-
-	// books.GET("/", bookHandler.BooksGetAll)
-	// books.GET("/:id", bookHandler.BooksGetById)
-	// books.GET("/query", handler.QueryHandler)
-	// books.POST("/", bookHandler.InsertBook)
-	// books.POST("/:id", bookHandler.UpdateBook)
 
 	router.Run(":8888")
 }
